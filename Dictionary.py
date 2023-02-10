ea84bf44f956d188be263055ccf274b93793e5a2
speedysam23 <sjglen23@gmail.com>
2023-02-10 16:15:07 +0000

    Fixes

--- Dictionary.py
+++ Dictionary.py
@@ -1,10 +1,14 @@
-cities = {"Nepal":"Kathmandu", "Italy": "Rome", "England": "London", "France":"Paris", "Germany":"Berlin","Japan":"Tokyo"}
+cities = {"nepal":"Kathmandu", "italy": "Rome", "england": "London", "france":"Paris", "germany":"Berlin","japan":"Tokyo"}
 x=1
 while x == 1:
     x=0
     CountryChoice = input("Please enter the name of a country:")
+    CountryChoice = CountryChoice.lower
     if (CountryChoice) in cities:
-     print("Yes")
+        print("Yes")
+        x=1
     elif (CountryChoice) not in cities:
-       print("No")
+        print("No")
+        x=1
+
 
