4be227b84fa243b7e2faffb1347d08a3e5e76321
speedysam23 <sjglen23@gmail.com>
2023-02-10 13:01:13 +0000

    Progressed on task

--- Monster OOP.py
+++ Monster OOP.py
@@ -1,32 +1,26 @@
+import random
 class Monster(object):
-    def __init__(self,Name, HP, Dialogue):
-        self.Name = Name
+    def __init__(self, name, Health, Dialogue):
+        self.name = name
 
-        self.HP = HP
+        self.Health = Health
 
         self.Dialogue = Dialogue
 
-    def get_Name(self):
-        return self.Name
-    def get_HP(self):
-        return self.HP
+    def get_name(self):
+        return self.name
+    def get_Health(self):
+        return self.Health
     def get_Dialogue(self):
         return self.Dialogue
     def describe (self):
-      print ("Name:", Monster.Name)
-      print("HP:", Monster.HP)
-      print("The monster exclaims:", Monster.Dialogue)
-    #def set_Name(self):
-      Name = input(("enter Name"))
-      self.Name = Name
-   # def set_HP(self):
-      HP = input(("enter HP"))
-      self.HP = HP  
-    #def set_Dialogue(self):
-      Dialogue = input(("enter Dialogue"))
-      self.Dialogue = Dialogue
-			
-
+      print("Name:", self.name)
+      print("Health:", self.Health)
+    def speak(self):
+      print("The monster exclaims:", self.Dialogue)
+    def damage(self):
+       print (self.Health - (random.randint(1,100)))
 Monster1 = Monster("Garry", 100, "aaaaa I'm big and scary, fear me human")
-
-Monster.describe(Monster1)
+Monster1.describe()
+Monster1.speak()
+Monster1.damage()
