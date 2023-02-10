332d010ea4e216916ae2c8c9c02c44acda3540f1
speedysam23 <sjglen23@gmail.com>
2023-02-10 13:15:30 +0000

    Added ally and monster subclasses

--- Monster OOP.py
+++ Monster OOP.py
@@ -1,5 +1,7 @@
 import random
-class Monster(object):
+class character(object):
+   print(" ")
+class Monster(character):
     def __init__(self, name, Health, Dialogue):
         self.name = name
 
@@ -20,7 +22,37 @@ class Monster(object):
       print("The monster exclaims:", self.Dialogue)
     def damage(self):
        print (self.Health - (random.randint(1,100)))
+
+class Ally(character):
+  def __init__(self, name, Health, Dialogue):
+      
+
+      self.name = name
+
+      self.Health = Health
+
+      self.Dialogue = Dialogue
+      
+#     Do I really need to define all these methods again????
+  def get_name(self):
+     return self.name
+  def get_Health(self):
+     return self.Health
+  def get_Dialogue(self):
+     return self.Dialogue
+  def describe (self):
+    print("Name:", self.name)
+    print("Health:", self.Health)
+  def speak(self):
+     print("Your companion bellows:", self.Dialogue)
+  def damage(self):
+     print (self.Health - (random.randint(1,100)))
 Monster1 = Monster("Garry", 100, "aaaaa I'm big and scary, fear me human")
 Monster1.describe()
 Monster1.speak()
 Monster1.damage()
+
+Ally1 = Ally("Hercules",1000, "I'LL SAVE YOU")
+Ally1.describe()
+Ally1.speak()
+Ally1.damage()
