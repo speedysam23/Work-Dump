c4fbf46d8458b675046dc4ce60a5e2a441c6e531
speedysam23 <sjglen23@gmail.com>
2023-03-30 23:03:19 +0100

    Completed Fizz Buzz

--- Counter controlled iteration.py
+++ Counter controlled iteration.py
@@ -1,10 +1,17 @@
-def fact():
-    fact = 1
-    num = int(input("Enter a number"))
-    for i in range(1,num+1):
-        fact = fact * i
-    print ("The factorial of",num, "is", fact)
+#def fact():
+#    fact = 1
+#    num = int(input("Enter a number"))
+#    for i in range(1,num+1):
+#        fact = fact * i
+#     print ("The factorial of",num, "is", fact)
+#fact()
 
-fact()
-
-
+for Counter in range(1,101,1):
+    if Counter % 5 == 0 and Counter %3 != 0:
+        print("Buzz")
+    if Counter % 3 == 0 and Counter %5 != 0:
+        print ("Fizz")
+    if Counter %5 == 0 and Counter % 3 == 0:
+        print("FizzBuzz")
+    elif Counter %5 != 0 and Counter % 3 != 0:
+        print (Counter)
