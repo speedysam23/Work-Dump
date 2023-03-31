9d75508df75a87261ee2bdfc6197404ca456682e
speedysam23 <sjglen23@gmail.com>
2023-03-31 12:25:02 +0100

    small changes

--- Counter controlled iteration.py
+++ Counter controlled iteration.py
@@ -3,7 +3,7 @@
 #    num = int(input("Enter a number"))
 #    for i in range(1,num+1):
 #        fact = fact * i
-#     print ("The factorial of",num, "is", fact)
+#    print ("The factorial of",num, "is", fact)
 #fact()
 
 for Counter in range(1,101,1):
